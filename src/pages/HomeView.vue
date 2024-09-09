5a97abced1d2b29b3407d3b373077fab5fa0cd54
goriori <vladislav.borisov2010@gmail.com>
2024-09-09 09:44:52 +0400

    init project

---
+++ src/pages/HomeView.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { useApplicationStore } from '@/stores/app.store.ts'
+import Button from '@/components/ui/button/Button.vue'
+import GetData from '@/features/get-data/GetData.vue'
+import TestForm from '@/components/ui/form/test/TestForm.vue'
+
+const applicationStore = useApplicationStore()
+
+const openTestModal = () => {
+  applicationStore.getModal('get-data-modal')?.onShow()
+}
+
+const openErrorAlert = () => {
+  applicationStore
+    .getAlert('error')
+    ?.setSettings({ message: 'open error' })
+    .onShow()
+}
+</script>
+
+<template>
+  <div class="page container">
+    <section class="page-top">top</section>
+    <section class="page-center">
+      <p>center</p>
+      <GetData />
+      <Button
+        title="open modal"
+        background="primary"
+        @click="openTestModal"
+      ></Button>
+      <Button
+        title="open error alert"
+        background="primary"
+        @click="openErrorAlert"
+      ></Button>
+      <TestForm />
+    </section>
+    <section class="page-bottom">bottom</section>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.page {
+  &-center {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+}
+
+.btn {
+  max-width: 200px;
+}
+</style>
